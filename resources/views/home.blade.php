f66e621e64e01dbfb6ddc8f3f7cc76c8664f5620
ishu <ishu10526@gmail.com>
2023-11-24 21:53:08 +0530

    create livewire component01

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    @livewireStyles
 </head>
 <body>
-    <h2>Welcome!!!</h2>
+    @livewire('component01')
+
+    @livewireScripts
 </body>
 </html>
